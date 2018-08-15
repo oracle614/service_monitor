08f82e6eb94f0e4cd8c1173226db3e0fcd5592bf
lvjj <lvjj@xinguangnet.com>
2018-08-15 10:21:17 +0800

    update

--- uwsgi.py
+++ uwsgi.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from api.service_monitor_result import PassResult, FailResult
+from api.service_monitor_alarm import Alarm
 app = Flask(__name__)
 api = Api(app)
 
 
-
 api.add_resource(PassResult, '/monitor/passresult')
 api.add_resource(FailResult, '/monitor/failresult')
+api.add_resource(Alarm, '/monitor/alarm')
